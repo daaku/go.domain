689e05e5e74e02c1775cd59418f2d4c78d9cddf3
Naitik Shah <n@daaku.org>
2012-05-23 17:30:23 -0700

    initial commit

---
+++ domain.go
@@ -0,0 +1,74 @@
+// Package domain allows for extracting the TLD.
+//
+// This is adapted from the PHP source at:
+//   http://www.dkim-reputation.org/regdom-libs/
+package domain
+
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	star = "*"
+	bang = "!"
+)
+
+var (
+	empty = tld{}
+	pop   = tld{bang: empty}
+)
+
+type tld map[string]tld
+
+// Extract the TLD.
+func TLD(domain string) (string, error) {
+	registered, err := Registered(domain)
+	if err != nil {
+		return "", err
+	}
+	return registered[strings.Index(registered, ".")+1:], nil
+}
+
+// Extract the base domain, including the TLD.
+func Registered(domain string) (string, error) {
+	parts := strings.Split(domain, ".")
+	partsLen := len(parts)
+	if partsLen < 2 || parts[partsLen-2] == "" {
+		return "", fmt.Errorf("Invalid domain structure: %s", domain)
+	}
+	registered := find(parts, root)
+	if registered == "" || strings.Index(registered, ".") < 0 {
+		return "", fmt.Errorf("Invalid domain: %s", domain)
+	}
+	return registered, nil
+}
+
+func find(parts []string, current tld) string {
+	const popChar = "#"
+	partsLen := len(parts)
+	part := ""
+	if partsLen > 0 {
+		part = parts[partsLen-1]
+	}
+	if current == nil {
+		return part
+	}
+	if current[bang] != nil {
+		return popChar
+	}
+	next := current[part]
+	if next == nil {
+		next = current[star]
+	}
+	if next == nil {
+		return part
+	}
+	rest := find(parts[:partsLen-1], next)
+	if rest == popChar {
+		return part
+	} else if len(rest) > 0 {
+		return rest + "." + part
+	}
+	return ""
+}
---
+++ domain_test.go
@@ -0,0 +1,102 @@
+package domain_test
+
+import (
+	"github.com/nshah/go.domain"
+	"testing"
+)
+
+var validRegistered = map[string]string{
+	".fbrell.com":                    "fbrell.com",
+	"local.fbrell.com":               "fbrell.com",
+	"www.fbrell.com":                 "fbrell.com",
+	"fbrell.com":                     "fbrell.com",
+	"fbrell.edu":                     "fbrell.edu",
+	"www.fbrell.edu":                 "fbrell.edu",
+	"registered.com":                 "registered.com",
+	"sub.registered.com":             "registered.com",
+	"parliament.uk":                  "parliament.uk",
+	"sub.registered.valid.uk":        "registered.valid.uk",
+	"registered.somedom.kyoto.jp":    "registered.somedom.kyoto.jp",
+	"sub.academy.museum":             "sub.academy.museum",
+	"subsub.sub.academy.museum":      "sub.academy.museum",
+	"sub.nic.pa":                     "nic.pa",
+	"registered.sb":                  "registered.sb",
+	"sub.registered.sb":              "registered.sb",
+	"subsub.registered.something.zw": "registered.something.zw",
+	"subsub.registered.9.bg":         "registered.9.bg",
+	"registered.co.bi":               "registered.co.bi",
+	"sub.registered.bi":              "registered.bi",
+	"subsub.registered.ee":           "registered.ee",
+}
+
+var validTLD = map[string]string{
+	".fbrell.com":                    "com",
+	"local.fbrell.com":               "com",
+	"www.fbrell.com":                 "com",
+	"fbrell.com":                     "com",
+	"fbrell.edu":                     "edu",
+	"www.fbrell.edu":                 "edu",
+	"registered.com":                 "com",
+	"sub.registered.com":             "com",
+	"parliament.uk":                  "uk",
+	"sub.registered.valid.uk":        "valid.uk",
+	"registered.somedom.kyoto.jp":    "somedom.kyoto.jp",
+	"sub.academy.museum":             "academy.museum",
+	"subsub.sub.academy.museum":      "academy.museum",
+	"sub.nic.pa":                     "pa",
+	"registered.sb":                  "sb",
+	"sub.registered.sb":              "sb",
+	"subsub.registered.something.zw": "something.zw",
+	"subsub.registered.9.bg":         "9.bg",
+	"registered.co.bi":               "co.bi",
+	"sub.registered.bi":              "bi",
+	"subsub.registered.ee":           "ee",
+}
+
+var invalidDomains = []string{
+	"www.fbrell.foo",
+	"local.fbrell.com:43600",
+	"invalid-fqdn",
+	"org",
+	"academy.museum",
+}
+
+func TestValidRegistered(t *testing.T) {
+	for d, expected := range validRegistered {
+		actual, err := domain.Registered(d)
+		if err != nil {
+			t.Errorf(`Error in Registered for domain "%s": %s`, d, err)
+			continue
+		}
+		if expected != actual {
+			t.Errorf(
+				`Failed Registered for domain "%s". Was expecting "%s" but got "%s"`,
+				d, expected, actual)
+		}
+	}
+}
+
+func TestValidTLD(t *testing.T) {
+	for d, expected := range validTLD {
+		actual, err := domain.TLD(d)
+		if err != nil {
+			t.Errorf(`Error in TLD for domain "%s": %s`, d, err)
+			continue
+		}
+		if expected != actual {
+			t.Errorf(
+				`Failed TLD for domain "%s". Was expecting "%s" but got "%s"`,
+				d, expected, actual)
+		}
+	}
+}
+
+func TestInvalid(t *testing.T) {
+	for _, d := range invalidDomains {
+		actual, err := domain.Registered(d)
+		if err == nil {
+			t.Errorf(`Was expecting error for invalid domain "%s" but got "%s"`,
+				d, actual)
+		}
+	}
+}
